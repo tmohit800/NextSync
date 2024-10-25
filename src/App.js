b3ebff3768870e7fb441bb449668bb0908945297
tmohit800 <tmohit800@gmail.com>
2024-10-26 01:19:17 +0530

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./styles.scss";
 
 function App() {
   return (
-    <div className="App">
-      Task List Manager Start
+    <div className="app-container">
+      <div className="header">Header</div>
+      <div className="content">
+        <div className="sidebar">Sidebar</div>
+        <div className="section">
+          <div className="section-panel">Section Panel</div>
+          <div className="footer">Footer</div>
+        </div>
+      </div>
     </div>
   );
 }
